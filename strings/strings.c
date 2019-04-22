2a1ed0f90de9cc8a44105d0651b2c1800d3f9c39
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-22 17:41:43 -0500

    Completed strings

--- strings/strings.c
+++ strings/strings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
     Given a character array s (as a pointer), return the number of 
@@ -9,13 +10,11 @@
 */
 int string_length(char *s)
 {
-    printf("%s\n", s);
     int length = 0;
 
     while (s[length] != '\0')
     {
         length++;
-        printf("%i\n", length);
     }
 
     return length;
@@ -27,20 +26,30 @@ int string_length(char *s)
     enough space for the reversed string. Don't forget to terminate 
     the reversed string with a null character. Return the rv array.
 */
-// char *reverse_string(char *rv, char *s)
-// {
-// }
+char *reverse_string(char *rv, char *s)
+{
+    // char str[100] = "String Reversed";
+    int len = strlen(s) - 1;
+    int start = 0;
+    int end = len;
+
+    for (start = 0; start <= len; start++, end--)
+    {
+        rv[start] = s[end];
+    }
+    return rv;
+}
 
 // #ifndef TESTING
 int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
-    // char quote2[] = "a man a plan a canal panama";
+    char quote2[] = "a man a plan a canal panama";
 
-    // char rv[512];
+    char rv[512];
 
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
-    // printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
+    printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
 
     return 0;
 }
